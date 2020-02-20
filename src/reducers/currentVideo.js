c18d9c714c6392c99c816b71d4f82f141e3235c3
jscholar <jinruncai@gmail.com>
2020-02-20 14:26:57 -0800

    Create reducers for changing videoList and currentVideo states

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,8 +1,16 @@
 import Redux from 'redux';
 
-var currentVideoReducer = (state = null, action) => {
-  //TODO: define a reducer for the currentVideo field of our state.
-  return state;
+var currentVideoReducer = (state = {}, action) => {
+  switch (action.type) {
+
+  case 'changeCurrentVideo':
+    return {
+      currentVideo: action.payload.video
+    };
+  default:
+    return state;
+
+  }
 };
 
 export default currentVideoReducer;
--- src/reducers/videoList.js
+++ src/reducers/videoList.js
@@ -2,7 +2,16 @@ import Redux from 'redux';
 
 var videoListReducer = (state = [], action) => {
   //TODO: define a reducer for the videoList field of our state.
-  return state;
+  switch (action.type) {
+
+  case 'changeVideoList':
+    return {
+      videos: action.payload.videos
+    };
+  default:
+    return state;
+    
+  }
 };
 
 export default videoListReducer;
