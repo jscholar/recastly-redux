1158339c3c4e07f96c6a9236274474fde01ea165
jscholar <jinruncai@gmail.com>
2020-02-20 14:29:57 -0800

    Create action objects for changing videoList and currentVideo

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,5 +1,9 @@
 var changeVideo = (video) => ({
   //TODO:  Return some action object to change the currently playing video.
+  type: 'changeCurrentVideo',
+  payload: {
+    video: video
+  }
 });
 
 export default changeVideo;
--- src/actions/videoList.js
+++ src/actions/videoList.js
@@ -1,5 +1,9 @@
 var changeVideoList = (videos) => ({
   //TODO:  Return some action object to change the list of videos in VideoList.
+  type: 'changeVideoList',
+  payload: {
+    videos: videos
+  }
 });
 
 export default changeVideoList;
