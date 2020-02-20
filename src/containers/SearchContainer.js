e761eed3e7f1b8578f2a4c8b38053ada13517034
jscholar <jinruncai@gmail.com>
2020-02-20 14:43:49 -0800

    Wrap Search component with high order redux component
    Gives search a dispatch to handle searches

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -2,10 +2,17 @@ import { connect } from 'react-redux';
 import Search from './../components/Search.js';
 import handleSearchChange from '../actions/search.js';
 
-var SearchContainer = () => {};
+// dispatchers with your search component props.
+const mapDispatchToProps = {
+  handleSearchChange
+};
 
 //TODO: define a SearchContainer component which will hook up your action
-// dispatchers with your search component props.
+var SearchContainer = connect(null, mapDispatchToProps)(Search);
+
+
+
+
 //HINT: use react-redux 'connect' method to generate a container component from
 //state and dispatch mappings.
 
