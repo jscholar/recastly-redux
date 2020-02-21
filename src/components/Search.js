1c6a93fbe8706f390a50daaebec673d212d0e19a
jscholar <jinruncai@gmail.com>
2020-02-20 16:30:36 -0800

    Add click and 'enter' key functionality to search

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,19 +4,38 @@ import React from 'react';
 class Search extends React.Component {
   constructor(props) {
     super(props);
+    this.handleSearchChange = _.throttle(
+      this.props.handleSearchChange,
+      1500,
+      {leading: false}
+    );
   }
 
   render() {
     return (
       <div className="search-bar form-inline">
         <input
+        
           className="form-control"
           type="text"
-          onChange={() => {
-            handleSearchChange($('.form-control').val());
-          }}
+
+          onKeyPress={
+            (event) => {
+              if (event.key === 'Enter') {
+                this.handleSearchChange.flush();
+              }
+            }
+          }
+
+          onChange={
+            () => this.handleSearchChange($('.form-control').val())
+          }
+
         />
-        <button className="btn hidden-sm-down">
+        <button onClick={
+          this.handleSearchChange.flush
+        }
+        className="btn hidden-sm-down">
           <span className="glyphicon glyphicon-search"></span>
         </button>
       </div>
